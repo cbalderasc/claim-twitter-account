a6d6f146efaa7d18bd0ff211e8724b77bdd00eef
Ivan Tovar <ivxn2309@gmail.com>
2021-07-09 04:05:54 -0500

    NCTA-1 NCTA-9 NCTA New index view

--- src/load.js
+++ src/load.js
@@ -3,6 +3,19 @@ document.getElementById('username-list').onchange = function(){
     var reader = new FileReader();
     reader.onload = function(progressEvent){
         var usernames = this.result.split('\n');
+        let list = document.getElementById("list-rendered-inner");
+
+        for(var i = 0; i < usernames.length; i++) {
+            let element = document.createElement('li');
+            element.innerHTML = usernames[i];
+            list.append(element);
+        }
+
+        let button = document.createElement('button');
+        button.setAttribute("id", "process-list");
+        button.innerText = "Procesar";
+        document.getElementById("list-rendered").append(button);
+        document.getElementById("list-rendered").setAttribute("style", "display: block");
     };
     reader.readAsText(file);
 };
